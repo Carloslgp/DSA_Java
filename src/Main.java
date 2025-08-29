5e5aa09e5df36ddc4fcc6279cd72463f9959c8b9
Carlos Leonardo <carloslgp3585@gmail.com>
2025-08-29 20:39:14 -0300

    Add Queue implementation

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,17 @@
-import ArrayList.MyArrayList;
-import Stack.MyStack;
-
-import java.util.Arrays;
-
-
+import Queue.MyQueue;
 
 public class Main {
     public static void main(String[] args) {
-        MyStack stack = new MyStack();
-        stack.push(5);
-        stack.push(6);
-        stack.print();
-        System.out.println();
-        System.out.println(stack.pop());
-        System.out.println();
-        stack.print();
-        System.out.println();
-        stack.push(7);
-        stack.push(8);
-        stack.push(9);
-        System.out.println(stack.isEmpty());
-        System.out.println();
-        System.out.println(stack.pop());
-        System.out.println();
-        stack.print();
-        System.out.println();
-        stack.clear();
-        System.out.println(stack.isEmpty());
-        System.out.println();
-        stack.print();
-
+        MyQueue<Integer> myQueue = new MyQueue<Integer>();
+        myQueue.enqueue(1);
+        myQueue.enqueue(2);
+        myQueue.enqueue(3);
+        myQueue.enqueue(4);
+        myQueue.enqueue(5);
+        myQueue.enqueue(6);
+        myQueue.dequeue();
+        myQueue.dequeue();
+        myQueue.print();
 
     }
 }
