2ffa1044693a15ef2b114977c115c66478df0ae1
Carlos Leonardo <carloslgp3585@gmail.com>
2025-09-27 08:52:19 -0300

    Feat: Add BinaryTree search and remove methods

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -1,5 +1,5 @@
 package BinaryTree;
-import javax.swing.*;
+
 
 
 public class BinaryTree<T extends Comparable<T>>{
@@ -40,9 +40,136 @@ public class BinaryTree<T extends Comparable<T>>{
     }
 
 
+    public TreeNode<T> search(T data) {
+
+        if(root == null){
+            System.out.println("Tree is empty");
+            return new TreeNode<T>();
+        }
+
+        if(root.getData().compareTo(data) == 0){
+            return root;
+        }
+
+        return search(new TreeNode<>(data), root);
+
+
+    }
+
+    public TreeNode<T> search(TreeNode<T> nodeToSearch,  TreeNode<T> tempNode){
+        if(tempNode.getRight() == null && tempNode.getLeft() == null && tempNode.getData().compareTo(nodeToSearch.getData()) != 0){
+            System.out.println("Not found");
+            return null;
+        }else{
+
+            //right
+            if(tempNode.getData().compareTo(nodeToSearch.getData()) < 0){
+                return search(nodeToSearch, tempNode.getRight());
+
+
+            //left
+            }else  if(tempNode.getData().compareTo(nodeToSearch.getData()) > 0){
+                return search(nodeToSearch, tempNode.getLeft());
+            }else{
+                System.out.println("Found: " + tempNode.getData());
+                return tempNode;
+            }
+
+        }
+
+    }
+
+
+
+
+
+    public void remove(T data) {
+        TreeNode<T> nodeToRemove = search(data);
+
+        if(nodeToRemove == null) {
+            System.out.println("Not found");
+            return;
+        }
+        else if(nodeToRemove.getData() == null){
+            System.out.println("Tree is empty");
+            return;
+        }else{
+            if(nodeToRemove.getRight() == null && nodeToRemove.getLeft() == null){
+                removeNoChild(nodeToRemove);
+            }else if(nodeToRemove.getRight() == null || nodeToRemove.getLeft() == null){
+                removeOneChild(nodeToRemove);
+            }else{
+                removeTwoChild(nodeToRemove);
+            }
+        }
+
+
+
+
+
+
+    }
+
+    private void removeNoChild(TreeNode<T> NodeToRemove){
+        TreeNode<T> parentNode = findParent(NodeToRemove, root);
+        if(parentNode.getRight().equals(NodeToRemove)){
+            parentNode.nullRight();
+        }
+        else if(parentNode.getLeft().equals(NodeToRemove)){
+            parentNode.nullLeft();
+        }
+        NodeToRemove.setData(null);
+
+    }
+
+    private void removeOneChild(TreeNode<T> NodeToRemove){
+
+
+    }
+
+    private void removeTwoChild(TreeNode<T> NodeToRemove){
+
+
+    }
+
+
+    private TreeNode<T> findParent(TreeNode<T> nodeToFind ,TreeNode<T> tempNode){
+        if(tempNode.getRight().equals(nodeToFind) || tempNode.getLeft().equals(nodeToFind)){
+            return tempNode;
+        }else{
+            if(tempNode.getData().compareTo(nodeToFind.getData()) < 0){
+                return findParent(nodeToFind, tempNode.getRight());
+            }else{
+                return findParent(nodeToFind, tempNode.getLeft());
+            }
+        }
+    }
 
     public TreeNode<T> getRoot() {
         return root;
+    }
+
+
+    public void printTree() {
+        printTree(root, 0);
+    }
+
+    private void printTree(TreeNode<T> node, int level) {
+        if (node == null) {
+            return;
+        }
+
+
+        printTree(node.getRight(), level + 1);
+
+
+        for (int i = 0; i < level; i++) {
+            System.out.print("    ");
+        }
+        System.out.println(node.getData());
+
+
+        printTree(node.getLeft(), level + 1);
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,18 @@ public class Main {
     public static void main(String[] args) {
         BinaryTree<Integer> tree = new BinaryTree<Integer>(10);
 
-        tree.insert(5);
+        tree.insert(8);
+        tree.insert(2);
+        tree.insert(1);
         tree.insert(3);
+        tree.insert(9);
         tree.insert(11);
-        tree.insert(10);
+        tree.insert(12);
+
+        tree.remove(3);
+
+
+        tree.printTree();
 
 
     }
